f2bb8cc9aac28d6d3d46e90653e5cb1e0fcbab7c
Brenton Morris <brenton.morris@odinsol.com>
2015-08-20 17:14:27 +1200

    .

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT=80;
 
 //We need a function which handles requests and send response
 function handleRequest(request, response){
-    response.end('It Works!!! Path Hit: ' + request.url);
+    response.end('It Works! Path Hit: ' + request.url);
 }
 
 //Create a server
